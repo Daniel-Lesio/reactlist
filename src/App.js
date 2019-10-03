8b4172ea4e3fe4efdb7998c79717191dabaa1672
Daniel Lesio <lesiewiczsales@gmail.com>
2019-10-03 22:57:36 +0200

    'list'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Todos from  './components/Todos'
+import AddTodo from './components/AddTodo'
+import Navbar from './components/Navbar'
+import './App.css'
+export default class App extends Component {
+  state={
+    todos : [
+      { id : 1, content : 'Zrób coś' },
+      { id : 2, content : 'Zrób coś jeszcze'}
+    ]
+  }
+  deleteTodo = (id) =>{
+      const todos = this.state.todos.filter(todo=>{
+        return todo.id !== id
+    })
+    this.setState({
+      todos : todos
+    })
+  }
+  addTodo = (todo) =>{
+    todo.id = this.state.todos[this.state.todos.length-1].id + 1
+    let todos = [ ...this.state.todos , todo  ]
+    this.setState({
+      todos
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+      <div className='app container ' >
+      <h1 className='center deep-purple-text ' > Lista </h1>
+        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } ></Todos>        
+        <AddTodo addTodo={this.addTodo}></AddTodo>
+      </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+        <div className="deep-purple nav-wrapper navbar-fixed">
+          <div href="#" className="brand-logo center">Twoja Lista</div>
+      
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
